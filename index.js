64f5edea71753d9f9cb4279e745efbcf691a0a15
Mark Barr <mark@hyperingenuity.com>
2019-07-29 16:56:37 -0400

    smtp: option handling to create server, initial event handlers

--- index.js
+++ index.js
@@ -1,10 +1,74 @@
 'use strict';
 
+const fs = require('fs');
 const { SMTPServer } = require('smtp-server');
 
 function SMTP(skyfall, options) {
   this.id = skyfall.utils.id();
-  this.server = new SMTPServer(options);
+
+  this.callbacks = {
+    onAuth: null,
+    onConnect: null,
+    onMailFrom: null,
+    onRcptTo: null
+  };
+
+  const onAuth = (auth, session, callback) => {
+    if (this.callbacks.onAuth && typeof this.callbacks.onAuth === 'function') {
+      return this.callbacks.onAuth(auth, session, callback);
+    }
+    return callback(null, { user: auth.username });
+  };
+
+  const onConnect = (session, callback) => {
+    console.log(session.remoteAddress);
+    if (this.callbacks.onConnect && typeof this.callbacks.onConnect === 'function') {
+      return this.callbacks.onConnect(session, callback);
+    }
+    return callback();
+  };
+
+  const onMailFrom = (address, session, callback) => {
+    if (this.callbacks.onMailFrom && typeof this.callbacks.onMailFrom === 'function') {
+      return this.callbacks.onMailFrom(address, session, callback);
+    }
+    return callback();
+  };
+
+  const onRcptTo = (address, session, callback) => {
+    if (this.callbacks.onRcptTo && typeof this.callbacks.onRcptTo === 'function') {
+      return this.callbacks.onRcptTo(address, session, callback);
+    }
+    return callback();
+  };
+
+  this.server = new SMTPServer({
+    secure: options.secure || false,
+    key: options.key ? fs.readFileSync(options.key) : null,
+    certificate: options.certificate ? fs.readFileSync(options.certificate) : null,
+    name: options.name || 'skyfall-smtp-server',
+    size: Number(options.size) || 10485760, // 10MB
+    authOptional: options.authOptional || true,
+    allowInsecureAuth: options.allowInsecureAuth || true,
+    disableReverseLookup: options.disableReverseLookup || false,
+    maxClients: options.maxClients || Infinity,
+    useProxy: options.useProxy || false,
+    lmtp: options.lmtp || false,
+    socketTimeout: Number(options.socketTimeout) || 60000, // 60s
+    closeTimeout: Number(options.closeTimeout) || 30000, // 30s
+    onAuth,
+    onConnect,
+    onMailFrom,
+    onRcptTo
+  });
+
+  this.server.on('error', (error) => {
+    skyfall.events.emit({
+      type: 'smtp:server:error',
+      data: error,
+      source: this.id
+    });
+  });
 }
 
 module.exports = {
